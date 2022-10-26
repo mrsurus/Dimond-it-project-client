894ceaa9639279b76a823a88080140d1752e1472
Mr Surus <mrsurusmk07@gmail.com>
2022-10-26 18:43:56 +0600

    course done

--- src/Component/Courses/Courses.js
+++ src/Component/Courses/Courses.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
 import CourseDetails from '../CourseDetails/CourseDetails';
 
@@ -9,10 +9,13 @@ const Courses = () => {
     return (
         
             <Row className='mt-5 mx-5 p-2 '>
-                <Col md={4}  >
-                    {
-                        data.map(c =>  <Link to={`/course/${c.id}`} key={c.id} ><li className='my-5 h4'>{c.name}</li></Link>)
+                <Col md={4} className='border h-50 bg-light rounded'  >
+                    <div className ='w-75 mx-auto '>
+                        {
+                        data.map(c =>  <Link className='text-decoration-none  ' to={`/course/${c.id}`} key={c.id} ><Button className='my-5 d-block w-100 '>< p className='ms-0 h4'>{c.name}</p></Button> </Link>)
                     }
+                    </div>
+                    
                 </Col>
                 <Col className='text-left' style={{display:'flex', flexWrap:'wrap', gap: 15,}}>
                    {
